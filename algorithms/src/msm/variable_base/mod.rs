b1253fa3f6f51279e1ec6cc2d6daa54adc136d5f
coinsccg <825076979@qq.com>
2022-07-12 09:55:02 +0800

    debug cuda;

--- algorithms/src/msm/variable_base/mod.rs
+++ algorithms/src/msm/variable_base/mod.rs
@@ -65,6 +65,7 @@ impl VariableBaseMSM {
                 }
             }
         }
+        eprintln!("-------------------------------------------------------------------------------------------vm2222");
         standard::msm_standard(bases, scalars)
     }
 }
