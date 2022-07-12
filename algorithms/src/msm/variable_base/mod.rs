3d71944ebf82b74ddc9663b41c251c1e927c5f6e
coinsccg <825076979@qq.com>
2022-07-12 16:31:26 +0800

    add gpu;

--- algorithms/src/msm/variable_base/mod.rs
+++ algorithms/src/msm/variable_base/mod.rs
@@ -51,8 +51,9 @@ impl VariableBaseMSM {
     ) -> G::Projective {
         if TypeId::of::<G>() == TypeId::of::<G1Affine>() {
             #[cfg(all(feature = "cuda", target_arch = "x86_64"))]
-            {   if index == 100 {
-                return standard::msm_standard(bases, scalars);
+            {
+                if index == 100 {
+                    return standard::msm_standard(bases, scalars);
                 }
                 if !HAS_CUDA_FAILED.load(Ordering::SeqCst) {
                     match cuda::msm_cuda(bases, scalars, index) {
