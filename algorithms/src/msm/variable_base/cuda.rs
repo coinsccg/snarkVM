dd614214a20d5a8962c9523334a5f3d3c5b99717
coinsccg <825076979@qq.com>
2022-07-15 16:54:54 +0800

    optimization gpu calculation;

--- algorithms/src/msm/variable_base/cuda.rs
+++ algorithms/src/msm/variable_base/cuda.rs
@@ -24,7 +24,8 @@ use snarkvm_utilities::BitIteratorBE;
 use rust_gpu_tools::{cuda, program_closures, Device, GPUError, Program};
 
 use std::{any::TypeId, path::Path, process::Command};
-use std::sync::RwLock;
+use std::sync::{RwLock, Arc};
+use std::sync::atomic::AtomicU32;
 
 
 
@@ -63,164 +64,164 @@ struct CudaAffine {
     x: Fq,
     y: Fq,
 }
-
-/// Generates the cuda msm binary.
-fn generate_cuda_binary<P: AsRef<Path>>(file_path: P, debug: bool) -> Result<(), GPUError> {
-    // Find the latest compute code values.
-    let nvcc_help = Command::new("nvcc").arg("-h").output()?.stdout;
-    let nvcc_output =
-        std::str::from_utf8(&nvcc_help).map_err(|_| GPUError::Generic("Missing nvcc command".to_string()))?;
-
-    // Generate the parent directory.
-    let mut resource_path = aleo_std::aleo_dir();
-    resource_path.push("./blst_377_cuda/");
-    std::fs::create_dir_all(resource_path)?;
-
-    // TODO (raychu86): Fix this approach to generating files. Should just read all files in the `blst_377_cuda` directory.
-    // Store the `.cu` and `.h` files temporarily for fatbin generation
-    let mut asm_cuda_path = aleo_std::aleo_dir();
-    let mut asm_cuda_h_path = aleo_std::aleo_dir();
-    asm_cuda_path.push("./blst_377_cuda/asm_cuda.cu");
-    asm_cuda_h_path.push("./blst_377_cuda/asm_cuda.h");
-
-    let mut blst_377_ops_path = aleo_std::aleo_dir();
-    let mut blst_377_ops_h_path = aleo_std::aleo_dir();
-    blst_377_ops_path.push("./blst_377_cuda/blst_377_ops.cu");
-    blst_377_ops_h_path.push("./blst_377_cuda/blst_377_ops.h");
-
-    let mut msm_path = aleo_std::aleo_dir();
-    msm_path.push("./blst_377_cuda/msm.cu");
-
-    let mut types_path = aleo_std::aleo_dir();
-    types_path.push("./blst_377_cuda/types.h");
-
-    let mut tests_path = aleo_std::aleo_dir();
-    tests_path.push("./blst_377_cuda/tests.cu");
-
-    // Write all the files to the relative path.
-    {
-        let asm_cuda = include_bytes!("./blst_377_cuda/asm_cuda.cu");
-        let asm_cuda_h = include_bytes!("./blst_377_cuda/asm_cuda.h");
-        std::fs::write(&asm_cuda_path, asm_cuda)?;
-        std::fs::write(&asm_cuda_h_path, asm_cuda_h)?;
-
-        let blst_377_ops = include_bytes!("./blst_377_cuda/blst_377_ops.cu");
-        let blst_377_ops_h = include_bytes!("./blst_377_cuda/blst_377_ops.h");
-        std::fs::write(&blst_377_ops_path, blst_377_ops)?;
-        std::fs::write(&blst_377_ops_h_path, blst_377_ops_h)?;
-
-        let msm = include_bytes!("./blst_377_cuda/msm.cu");
-        std::fs::write(&msm_path, msm)?;
-
-        let types = include_bytes!("./blst_377_cuda/types.h");
-        std::fs::write(&types_path, types)?;
-    }
-
-    // Generate the cuda fatbin.
-    let mut command = Command::new("nvcc");
-    command
-        .arg(asm_cuda_path.as_os_str())
-        .arg(blst_377_ops_path.as_os_str())
-        .arg(msm_path.as_os_str());
-
-    // Add the debug feature for tests.
-    if debug {
-        let tests = include_bytes!("./blst_377_cuda/tests.cu");
-        std::fs::write(&tests_path, tests)?;
-
-        command.arg(tests_path.as_os_str()).arg("--device-debug");
-    }
-
-    // Add supported gencodes
-    command
-        .arg("--generate-code=arch=compute_60,code=sm_60")
-        .arg("--generate-code=arch=compute_70,code=sm_70")
-        .arg("--generate-code=arch=compute_75,code=sm_75");
-
-    if nvcc_output.contains("compute_80") {
-        command.arg("--generate-code=arch=compute_80,code=sm_80");
-    }
-
-    if nvcc_output.contains("compute_86") {
-        command.arg("--generate-code=arch=compute_86,code=sm_86");
-    }
-
-    command
-        .arg("-fatbin")
-        .arg("-dlink")
-        .arg("-o")
-        .arg(file_path.as_ref().as_os_str());
-
-    eprintln!("\nRunning command: {:?}", command);
-
-    let status = command.status()?;
-
-    // Delete all the temporary .cu and .h files.
-    {
-        let _ = std::fs::remove_file(asm_cuda_path);
-        let _ = std::fs::remove_file(asm_cuda_h_path);
-        let _ = std::fs::remove_file(blst_377_ops_path);
-        let _ = std::fs::remove_file(blst_377_ops_h_path);
-        let _ = std::fs::remove_file(msm_path);
-        let _ = std::fs::remove_file(types_path);
-        let _ = std::fs::remove_file(tests_path);
-    }
-
-    // Execute the command.
-    if !status.success() {
-        return Err(GPUError::KernelNotFound(
-            "Could not generate a new msm kernel".to_string(),
-        ));
-    }
-
-    Ok(())
-}
-
-/// Loads the msm.fatbin into an executable CUDA program.
-fn load_cuda_program(device: &Device) -> Result<Program, GPUError> {
-    // let devices: Vec<_> = Device::all();
-    // let device = match devices.first() {
-    //     Some(device) => device,
-    //     None => return Err(GPUError::DeviceNotFound),
-    // };
-
-    // Find the path to the msm fatbin kernel
-    // let mut file_path = aleo_std::aleo_dir();
-    // file_path.push("./blst_377_cuda/msm.fatbin");
-
-    // If the file does not exist, regenerate the fatbin.
-    // if !file_path.exists() {
-    //     generate_cuda_binary(&file_path, false)?;
-    // }
-
-    let cuda_device = match device.cuda_device() {
-        Some(device) => device,
-        None => return Err(GPUError::DeviceNotFound),
-    };
-
-    // eprintln!(
-    //     "\nUsing '{}' as CUDA device with {} bytes of memory",
-    //     device.name(),
-    //     device.memory()
-    // );
-
-    // let cuda_kernel = std::fs::read(file_path.clone())?;
-    let cuda_kernel = include_bytes!("./blst_377_cuda/msm.fatbin");
-
-    // Load the cuda program from the kernel bytes.
-    let cuda_program = match cuda::Program::from_bytes(cuda_device, cuda_kernel) {
-        Ok(program) => program,
-        Err(err) => {
-            // Delete the failing cuda kernel.
-            // std::fs::remove_file(file_path)?;
-            return Err(err);
-        }
-    };
-
-    Ok(Program::Cuda(cuda_program))
-}
-
-/// Run the CUDA MSM operation for a given request.
+//
+// /// Generates the cuda msm binary.
+// fn generate_cuda_binary<P: AsRef<Path>>(file_path: P, debug: bool) -> Result<(), GPUError> {
+//     // Find the latest compute code values.
+//     let nvcc_help = Command::new("nvcc").arg("-h").output()?.stdout;
+//     let nvcc_output =
+//         std::str::from_utf8(&nvcc_help).map_err(|_| GPUError::Generic("Missing nvcc command".to_string()))?;
+//
+//     // Generate the parent directory.
+//     let mut resource_path = aleo_std::aleo_dir();
+//     resource_path.push("./blst_377_cuda/");
+//     std::fs::create_dir_all(resource_path)?;
+//
+//     // TODO (raychu86): Fix this approach to generating files. Should just read all files in the `blst_377_cuda` directory.
+//     // Store the `.cu` and `.h` files temporarily for fatbin generation
+//     let mut asm_cuda_path = aleo_std::aleo_dir();
+//     let mut asm_cuda_h_path = aleo_std::aleo_dir();
+//     asm_cuda_path.push("./blst_377_cuda/asm_cuda.cu");
+//     asm_cuda_h_path.push("./blst_377_cuda/asm_cuda.h");
+//
+//     let mut blst_377_ops_path = aleo_std::aleo_dir();
+//     let mut blst_377_ops_h_path = aleo_std::aleo_dir();
+//     blst_377_ops_path.push("./blst_377_cuda/blst_377_ops.cu");
+//     blst_377_ops_h_path.push("./blst_377_cuda/blst_377_ops.h");
+//
+//     let mut msm_path = aleo_std::aleo_dir();
+//     msm_path.push("./blst_377_cuda/msm.cu");
+//
+//     let mut types_path = aleo_std::aleo_dir();
+//     types_path.push("./blst_377_cuda/types.h");
+//
+//     let mut tests_path = aleo_std::aleo_dir();
+//     tests_path.push("./blst_377_cuda/tests.cu");
+//
+//     // Write all the files to the relative path.
+//     {
+//         let asm_cuda = include_bytes!("./blst_377_cuda/asm_cuda.cu");
+//         let asm_cuda_h = include_bytes!("./blst_377_cuda/asm_cuda.h");
+//         std::fs::write(&asm_cuda_path, asm_cuda)?;
+//         std::fs::write(&asm_cuda_h_path, asm_cuda_h)?;
+//
+//         let blst_377_ops = include_bytes!("./blst_377_cuda/blst_377_ops.cu");
+//         let blst_377_ops_h = include_bytes!("./blst_377_cuda/blst_377_ops.h");
+//         std::fs::write(&blst_377_ops_path, blst_377_ops)?;
+//         std::fs::write(&blst_377_ops_h_path, blst_377_ops_h)?;
+//
+//         let msm = include_bytes!("./blst_377_cuda/msm.cu");
+//         std::fs::write(&msm_path, msm)?;
+//
+//         let types = include_bytes!("./blst_377_cuda/types.h");
+//         std::fs::write(&types_path, types)?;
+//     }
+//
+//     // Generate the cuda fatbin.
+//     let mut command = Command::new("nvcc");
+//     command
+//         .arg(asm_cuda_path.as_os_str())
+//         .arg(blst_377_ops_path.as_os_str())
+//         .arg(msm_path.as_os_str());
+//
+//     // Add the debug feature for tests.
+//     if debug {
+//         let tests = include_bytes!("./blst_377_cuda/tests.cu");
+//         std::fs::write(&tests_path, tests)?;
+//
+//         command.arg(tests_path.as_os_str()).arg("--device-debug");
+//     }
+//
+//     // Add supported gencodes
+//     command
+//         .arg("--generate-code=arch=compute_60,code=sm_60")
+//         .arg("--generate-code=arch=compute_70,code=sm_70")
+//         .arg("--generate-code=arch=compute_75,code=sm_75");
+//
+//     if nvcc_output.contains("compute_80") {
+//         command.arg("--generate-code=arch=compute_80,code=sm_80");
+//     }
+//
+//     if nvcc_output.contains("compute_86") {
+//         command.arg("--generate-code=arch=compute_86,code=sm_86");
+//     }
+//
+//     command
+//         .arg("-fatbin")
+//         .arg("-dlink")
+//         .arg("-o")
+//         .arg(file_path.as_ref().as_os_str());
+//
+//     eprintln!("\nRunning command: {:?}", command);
+//
+//     let status = command.status()?;
+//
+//     // Delete all the temporary .cu and .h files.
+//     {
+//         let _ = std::fs::remove_file(asm_cuda_path);
+//         let _ = std::fs::remove_file(asm_cuda_h_path);
+//         let _ = std::fs::remove_file(blst_377_ops_path);
+//         let _ = std::fs::remove_file(blst_377_ops_h_path);
+//         let _ = std::fs::remove_file(msm_path);
+//         let _ = std::fs::remove_file(types_path);
+//         let _ = std::fs::remove_file(tests_path);
+//     }
+//
+//     // Execute the command.
+//     if !status.success() {
+//         return Err(GPUError::KernelNotFound(
+//             "Could not generate a new msm kernel".to_string(),
+//         ));
+//     }
+//
+//     Ok(())
+// }
+//
+// /// Loads the msm.fatbin into an executable CUDA program.
+// fn load_cuda_program(device: &Device) -> Result<Program, GPUError> {
+//     // let devices: Vec<_> = Device::all();
+//     // let device = match devices.first() {
+//     //     Some(device) => device,
+//     //     None => return Err(GPUError::DeviceNotFound),
+//     // };
+//
+//     // Find the path to the msm fatbin kernel
+//     // let mut file_path = aleo_std::aleo_dir();
+//     // file_path.push("./blst_377_cuda/msm.fatbin");
+//
+//     // If the file does not exist, regenerate the fatbin.
+//     // if !file_path.exists() {
+//     //     generate_cuda_binary(&file_path, false)?;
+//     // }
+//
+//     let cuda_device = match device.cuda_device() {
+//         Some(device) => device,
+//         None => return Err(GPUError::DeviceNotFound),
+//     };
+//
+//     // eprintln!(
+//     //     "\nUsing '{}' as CUDA device with {} bytes of memory",
+//     //     device.name(),
+//     //     device.memory()
+//     // );
+//
+//     // let cuda_kernel = std::fs::read(file_path.clone())?;
+//     let cuda_kernel = include_bytes!("./blst_377_cuda/msm.fatbin");
+//
+//     // Load the cuda program from the kernel bytes.
+//     let cuda_program = match cuda::Program::from_bytes(cuda_device, cuda_kernel) {
+//         Ok(program) => program,
+//         Err(err) => {
+//             // Delete the failing cuda kernel.
+//             // std::fs::remove_file(file_path)?;
+//             return Err(err);
+//         }
+//     };
+//
+//     Ok(Program::Cuda(cuda_program))
+// }
+//
+// /// Run the CUDA MSM operation for a given request.
 // fn handle_cuda_request(context: &mut CudaContext, request: &CudaRequest) -> Result<G1Projective, GPUError> {
 //     let mapped_bases: Vec<_> = crate::cfg_iter!(request.bases)
 //         .map(|affine| CudaAffine {
@@ -262,7 +263,7 @@ fn load_cuda_program(device: &Device) -> Result<Program, GPUError> {
 //
 //         let kernel_1 = program.create_kernel(
 //             &context.pixel_func_name,
-//             window_lengths.len(),
+//             window_lengths.len()/4,
 //             context.num_groups as usize,
 //         )?;
 //
@@ -317,8 +318,8 @@ fn load_cuda_program(device: &Device) -> Result<Program, GPUError> {
 //         + lowest;
 //     Ok(final_result)
 // }
-
-
+//
+//
 //
 // /// Initialize the cuda request handler.
 // fn initialize_cuda_request_handler(input: crossbeam_channel::Receiver<CudaRequest>, device: &Device) {
@@ -333,10 +334,8 @@ fn load_cuda_program(device: &Device) -> Result<Program, GPUError> {
 //                 program,
 //             };
 //             while let Ok(request) = input.recv() {
-//                 std::thread::spawn(move || {
-//                     let out = handle_cuda_request(&mut context, &request);
-//                     request.response.send(out).ok();
-//                 });
+//                 let out = handle_cuda_request(&mut context, &request);
+//                 request.response.send(out).ok();
 //             }
 //         }
 //         Err(err) => {
@@ -348,7 +347,7 @@ fn load_cuda_program(device: &Device) -> Result<Program, GPUError> {
 //         }
 //     }
 // }
-
+//
 // fn init_cuda_dispatch(index: usize) {
 //     if let Ok(mut dispatchers) = CUDA_DISPATCH.write() {
 //         if dispatchers.len() > 0 {
@@ -372,6 +371,48 @@ fn load_cuda_program(device: &Device) -> Result<Program, GPUError> {
 // }
 
 
+fn load_cuda_program(device: &Device) -> Result<Program, GPUError> {
+    // let devices: Vec<_> = Device::all();
+    // let device = match devices.first() {
+    //     Some(device) => device,
+    //     None => return Err(GPUError::DeviceNotFound),
+    // };
+
+    // Find the path to the msm fatbin kernel
+    // let mut file_path = aleo_std::aleo_dir();
+    // file_path.push("./blst_377_cuda/msm.fatbin");
+
+    // If the file does not exist, regenerate the fatbin.
+    // if !file_path.exists() {
+    //     generate_cuda_binary(&file_path, false)?;
+    // }
+
+    let cuda_device = match device.cuda_device() {
+        Some(device) => device,
+        None => return Err(GPUError::DeviceNotFound),
+    };
+
+    // eprintln!(
+    //     "\nUsing '{}' as CUDA device with {} bytes of memory",
+    //     device.name(),
+    //     device.memory()
+    // );
+
+    // let cuda_kernel = std::fs::read(file_path.clone())?;
+    let cuda_kernel = include_bytes!("./blst_377_cuda/msm.fatbin");
+
+    // Load the cuda program from the kernel bytes.
+    let cuda_program = match cuda::Program::from_bytes(cuda_device, cuda_kernel) {
+        Ok(program) => program,
+        Err(err) => {
+            // Delete the failing cuda kernel.
+            // std::fs::remove_file(file_path)?;
+            return Err(err);
+        }
+    };
+
+    Ok(Program::Cuda(cuda_program))
+}
 
 fn handle_cuda_request(context: &mut CudaContext, request: &CudaRequest,  index: usize) -> Result<G1Projective, GPUError> {
     let devices: Vec<_> = Device::all();
@@ -508,8 +549,10 @@ fn init_cuda_dispatch(index: usize) {
     }
 }
 
+
 lazy_static::lazy_static! {
     static ref CUDA_DISPATCH: RwLock<Vec<crossbeam_channel::Sender<CudaRequest>>> = RwLock::new(Vec::new());
+    // static ref CUDA_THREAD: Arc<AtomicU32>
 }
 
 pub(super) fn msm_cuda<G: AffineCurve>(
