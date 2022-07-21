de52273a9be7fdf55ab3b57a8e6f6f8ad204d2bb
coinsccg <825076979@qq.com>
2022-07-21 15:17:25 +0800

    add thread channel

--- dpc/src/posw/posw.rs
+++ dpc/src/posw/posw.rs
@@ -139,6 +139,7 @@ impl<N: Network> PoSWScheme<N> for PoSW<N> {
 
                     // Run one iteration of PoSW.
                     let proof = self.prove_once_unchecked(&mut circuit, block_template, terminator, rng, index)?;
+                    eprintln!("111111111111111111111111111111111111111111111111111111111111111111111111");
                     // Check if the updated block header is valid.
                     if self.verify(
                         block_template.block_height(),
